19a86ec3a888545f88a810ea1231982448a47ac0
mesut146 <mesutdogansoy@gmail.com>
2019-07-31 18:05:11 +0300

    add new option

--- src/math/Config.java
+++ src/math/Config.java
@@ -7,6 +7,7 @@ public abstract class Config {
     }
     public abstract static class mul{
         public static boolean simplify=true;
+        public static boolean distributeCons=true;
     }
     public abstract static class pow{
         public static boolean simplify=true;
