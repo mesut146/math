58faa84eeb6ff01e8f13f217105fd2ae51bb7f19
Mesut <mesutdogansoy@gmail.com>
2021-02-19 16:27:10 +0300

    refactor

--- src/main/java/com/mesut/math/Linear.java
+++ src/main/java/com/mesut/math/Linear.java
@@ -1,47 +1,73 @@
 package com.mesut.math;
 
+import com.mesut.math.core.cons;
+import com.mesut.math.core.func;
+import com.mesut.math.core.variable;
+
 //a*n+b
 public class Linear {
-    public int a, b;
+    public func a, b;
+    public variable v;
+    public static variable defaultVar = variable.n;
 
     public Linear(int a, int b) {
+        this.a = cons.of(a);
+        this.b = cons.of(b);
+        this.v = defaultVar;
+    }
+
+    public Linear(func a, func b) {
         this.a = a;
         this.b = b;
+        this.v = defaultVar;
+    }
+
+    public Linear(func a, func b, variable v) {
+        this.a = a;
+        this.b = b;
+        this.v = v;
     }
 
     public boolean isEven() {
-        return a % 2 == 0 && b % 2 == 0;
+        if (a.isConstant() && b.isConstant()) {
+            return a.asCons().isEven() && b.asCons().isEven();
+        }
+        return false;
     }
 
     public boolean isOdd() {
-        return a % 2 == 0 && b % 2 == 1;
+        if (a.isConstant() && b.isConstant()) {
+            return a.asCons().isEven() && b.asCons().isOdd();
+        }
+        return false;
     }
 
     public Linear mul(int k) {
-        return new Linear(a * k, b * k);
+        return new Linear(a.mul(k), b.mul(k));
     }
 
     public Linear div(int k) {
-        return new Linear(a / k, b / k);
+        return new Linear(a.div(k), b.div(k));
     }
 
     public Linear add(int k) {
-        return new Linear(a, b + k);
+        return new Linear(a, b.add(k));
     }
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        if (a != 1) {
-            if (a == -1) {
-                sb.append("-");
-            }
-            else {
-                sb.append(a);
-            }
+        if (v == null) {
+            v = defaultVar;
         }
-        sb.append("*n");
-        if (b > 0) {
+        StringBuilder sb = new StringBuilder();
+        if (a.is(-1)) {
+            sb.append("-");
+        }
+        else if (!a.is(1)) {
+            sb.append(a);
+        }
+        sb.append(v);
+        if (b.sign == 1) {
             sb.append("+");
         }
         sb.append(b);
